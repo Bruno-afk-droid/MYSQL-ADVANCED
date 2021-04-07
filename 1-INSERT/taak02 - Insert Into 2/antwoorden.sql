cc95a37a037dee6bb5f0f8d3a721cf6f05082427
Bruno Hofman <bruno.hofman@student.rocva.nl>
2021-04-07 12:44:03 +0200

    commit

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,5 @@
+INSERT INTO `studenten` (student_id,voornaam,achternaam,klas,geboortedatum,woonplaats) VALUES
+(8,'Jan','Vermeer','9C','2000-08-08','Amsterdam'),
+(9,'Jan2','Vermeer','9C','2000-08-08','Amsterdam'),
+(10,'Jan3','Vermeer','9C','2000-08-08','Amsterdam');
 
--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,9 +1,22 @@
 -- Opdracht 1
+UPDATE `studenten2`
+SET woonplaats = 'Amstelveen'
 
 -- Opdracht 2 
+UPDATE`studenten2`
+SET adres='Pieter Aertszstraat 44',postcode='1234EG'
+WHERE student_id >=8
 
 -- Opdracht 3
+UPDATE`studenten2`
+SET geboortedatum = '2000-11-12'
+WHERE voornaam = 'Marloes'
 
 -- Opdracht 4
+UPDATE `studenten2` SET klas='9A' WHERE voornaam='Jan'
+UPDATE `studenten2` SET klas='9C'WHERE voornaam='Mohammed'
 
 -- Opdracht 5
+UPDATE `studenten2`SET
+voornaam = 'Johannah' WHERE 
+voornaam = 'Johanna'
